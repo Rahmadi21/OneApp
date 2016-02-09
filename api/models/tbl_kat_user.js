5ebc23b28156446ab5f0f94c088758a4df390fe4
tsabit <tsabitg23@gmail.com>
2016-02-09 10:00:14 +0700

    server js

--- api/models/tbl_kat_user.js
+++ api/models/tbl_kat_user.js
@@ -22,5 +22,87 @@ module.exports = {
 			res.json(data);
 		}
 		});
-}
+	},
+
+	postKatUser : function (req, res){
+	
+		var id = req.body.id;
+		var kategori = req.body.kategori;
+		var data = {
+			"error":1,
+			"one_app":""
+		};
+		if(id && kategori){
+			connection.query("INSERT INTO tbl_kat_user VALUES(?,?)",[id, kategori], function (err, rows, fields){
+				if(!!err){
+					data["one_app"] = "error dalam menambahkan data";
+				}else{
+					data["error"] = 0;
+					data["one_app"] = "data berhasil ditambahkan";
+				}
+			
+			res.json(data);
+	});
+		
+			}else{
+		data["one_app"] = "Tolong lengkapi semua data (i.e : id, kategori)";
+		res.json(data);
+
+
+		}
+	},
+
+	putKatUser : function (req, res){
+		var id = req.body.id;
+		var kategori = req.body.kategori;
+		var data = {
+			"error":1,
+			"one_app":""
+		};
+		if(id && kategori){
+			connection.query("UPDATE tbl_kat_user SET kategori=? WHERE id=?",[kategori, id], function (err, rows, fields){
+				if(!!err){
+					data["one_app"] = "error mengupdate data";
+				}else{
+					data["error"] = 0;
+					data["one_app"] = "data berhasil di update";
+				}
+			
+			res.json(data);
+		});
+		
+		}
+		else{
+		data["one_app"] = "Tolong lengkapi semua data (i.e : id, kategori)";
+		res.json(data);
+
+
+		}
+	},
+
+	deleteKatUser : function (req, res){
+		var id = req.body.id;
+		var data = {
+			"error":1,
+			"one_app":""
+		};
+		if(!!id){
+			connection.query("DELETE FROM tbl_kat_user WHERE id=?",[id], function (err, rows, fields){
+				if(!!err){
+					data["tbl_kat_user"] = "error delete data";
+				}else{
+					data["error"] = 0;
+					data["tbl_kat_user"] = " Delete user sukses";
+
+				}
+				res.json(data);
+				
+			});
+
+			
+		}else{
+			data["one_app"] = "Tolong lengkapi semua data (i.e :id)";
+			res.json(data);
+			}
+	}
 }
--- api/server.js
+++ api/server.js
@@ -15,7 +15,6 @@ var connection = mysql.createConnection({
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-/*========================tbl_user=======================*/
 app.get('/', function (req, res){
 	var data = {
 		"Data":""
@@ -23,6 +22,8 @@ app.get('/', function (req, res){
 	data["Data"] = "Selamat datang di OneApp";
 	res.json(data);
 });
+
+/*========================tbl_user=======================*/
 
 app.get('/tbl_user', userModel.getuser);
 
@@ -31,99 +32,18 @@ app.post('/tbl_user', userModel.postUser);
 app.put('/tbl_user', userModel.putUser); 
 
 app.delete('/tbl_user', userModel.deleteUser);
-
+/*--------------------------------------------------------*/
 
 /*========================tbl_kat_user=======================*/
-app.get('/', function (req, res){
-	var data = {
-		"Data":""
-	};
-	data["Data"] = "Selamat datang di OneApp";
-	res.json(data);
-});
 
-app.get('/tbl_kat_user', katUserModel.getKatUser)
+app.get('/tbl_kat_user', katUserModel.getKatUser);
 
-app.post('/tbl_kat_user', function (req, res){
-	
+app.post('/tbl_kat_user', katUserModel.postKatUser);
 
-	var id = req.body.id;
-	var kategori = req.body.kategori;
-	var data = {
-		"error":1,
-		"one_app":""
-	};
-	if(id && kategori){
-		connection.query("INSERT INTO tbl_kat_user VALUES(?,?)",[id, kategori], function (err, rows, fields){
-			if(!!err){
-				data["one_app"] = "error dalam menambahkan data";
-			}else{
-				data["error"] = 0;
-				data["one_app"] = "data berhasil ditambahkan";
-			}
-		
-		res.json(data);
-});
-	
-		}else{
-	data["one_app"] = "Tolong lengkapi semua data (i.e : id, kategori)";
-	res.json(data);
+app.put('/tbl_kat_user', katUserModel.putKatUser);
 
-
-	}
-});
-app.put('/tbl_kat_user', function (req, res){
-	var id = req.body.id;
-	var kategori = req.body.kategori;
-	var data = {
-		"error":1,
-		"one_app":""
-	};
-	if(id && kategori){
-		connection.query("UPDATE tbl_kat_user SET kategori=? WHERE id=?",[kategori, id], function (err, rows, fields){
-			if(!!err){
-				data["one_app"] = "error mengupdate data";
-			}else{
-				data["error"] = 0;
-				data["one_app"] = "data berhasil di update";
-			}
-		
-		res.json(data);
-});
-	
-	}else{
-	data["one_app"] = "Tolong lengkapi semua data (i.e : id, kategori)";
-	res.json(data);
-
-
-	}
-}); 
-
-app.delete('/tbl_kat_user', function (req, res){
-	var id = req.body.id;
-	var data = {
-		"error":1,
-		"one_app":""
-	};
-	if(!!id){
-		connection.query("DELETE FROM tbl_kat_user WHERE id=?",[id], function (err, rows, fields){
-			if(!!err){
-				data["tbl_kat_user"] = "error delete data";
-			}else{
-				data["error"] = 0;
-				data["tbl_kat_user"] = " Delete user sukses";
-
-			}
-			res.json(data);
-			
-		});
-
-	
-}else{
-	data["one_app"] = "Tolong lengkapi semua data (i.e :id)";
-	res.json(data);
-	}
-});
+app.delete('/tbl_kat_user', katUserModel.deleteKatUser);
+/*--------------------------------------------------------*/
 
 
 http.listen(8080,function(){
