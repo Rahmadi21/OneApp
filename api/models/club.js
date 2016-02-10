a8d1ba18500e46bc9b04ad00333fa0c81c81499e
Rahmadi <rahmadi.rpla@gmail.com>
2016-02-10 11:41:08 +0700

    nih yang clubnya udah

--- api/models/club.js
+++ api/models/club.js
@@ -4,17 +4,101 @@ var conn 		= require('../config/conn.js')
 var connection  = mysql.createConnection(conn);
 
 module.exports = {
-	getClub : function () {}
+	getClub : function (req, res){
+	
+	var data = {
+		"error": 1,
+		"one_app":""
+	};
+	connection.query("SELECT * from tbl_konten WHERE id_kat_konten='f1689864-cf1d-11e5-8978-b888e391'", [req.params.id], function (err, rows, fields){
+		if(rows.length !=0){
+			data["error"] = 0;
+			data["one_app"] = rows;
+			res.json(data);
+		}else{
+			data["one_app"] = 'tidak ditemukan';
+			res.json(data);
+		}
+		});
+
+	}
 
 	,
 
-	postClub :function () {}
+	postClub : function (req,res){
+	var Id = uuID.v4();
+	var Id_Kat_Konten = req.body.id_kat_konten;
+	var Id_User = req.body.id_user;
+	var Tgl_Posting = req.body.tgl_posting;
+	var Judul = req.body.judul;
+	var Isi = req.body.isi;
+	var Status = req.body.status;
+	var data = {
+		"error":1,
+		"data":""
+	};
+	if(Id && Id_Kat_Konten && Id_User && Tgl_Posting && Judul && Isi && Status){
+		con.query("INSERT INTO tbl_konten VALUES(?,?,?,?,?,?,?)",[Id,Id_Kat_Konten,Id_User,Tgl_Posting,Judul,Isi,Status],function (err, rows, fields){
+			if(!!err){
+				data["data"] = err;
+			}else{
+				data["error"] = 0;
+			}
+			res.json(data);
+		});
+	}else{
+		data["data"] = "Please provide all required data";
+		res.json(data);
+	}}
 
 	,
 
-	putClub  :function () {}
+	putClub  : function (req,res){
+	var Id = req.body.id;
+	var Id_Kat_Konten = req.body.id_kat_konten;
+	var Id_User = req.body.id_user;
+	var Tgl_Posting = req.body.tgl_posting;
+	var Judul = req.body.judul;
+	var Isi = req.body.isi;
+	var Status = req.body.status;
+	var data = {
+		"error":1,
+		"data":""
+	};
+	if(Id && Id_Kat_Konten && Id_User && Tgl_Posting && Judul && Isi && Status){
+		con.query("UPDATE tbl_konten SET id_kat_konten=?, id_user=?, tgl_posting=?, judul=?, isi=?, status=? WHERE id=?",[Id_Kat_Konten,Id_User,Tgl_Posting,Judul,Isi,Status,Id],function (err, rows, fields){
+			if(!!err){
+				data["data"] = err;
+			}else{
+				data["error"] = 0;
+			}
+			res.json(data);
+		});
+	}else{
+		data["data"] = "Please provide all required data";
+		res.json(data);
+	}}
+
 
 	,
 
-	deleteClub :function () {}
+	deleteClub : function (req,res){
+	var Id = req.body.id;
+	var data = {
+		"error":1,
+		"Users":""
+	};
+	if(!!Id){
+		con.query("DELETE FROM tbl_konten WHERE id_konten=?",[Id],function (err, rows, fields){
+			if(!!err){
+				data["data"] = err;
+			}else{
+				data["error"] = 0;
+			}
+			res.json(data);
+		});
+	}else{
+		data["data"] = "Please provide all required data";
+		res.json(data);
+	}}
 }
