75223426effdf584115ee2ee0c5e48549dbdbb5e
tsabit <tsabitg23@gmail.com>
2016-02-26 13:34:18 +0700

    nih jak

--- controller/loginCtrl.js
+++ controller/loginCtrl.js
@@ -1,9 +1,17 @@
-angular.module('myApp.login', ['myApp.loginService'])
-.controller('loginCtrl', function ($scope, $state, Login,ErrMsg, $mdDialog) {
+angular.module('myApp.login', ['myApp.loginService','ngCookies'])
+.controller('loginCtrl', function ($scope, $state, Login,ErrMsg, $mdDialog,$cookies) {
     $scope.login = function () {
         Login.login($scope.login).then(function (data) {
             if (Object.keys(data.data.detail).length === 1) {
                 $state.go('home');
+                var input = {
+                'email' : data.data.detail.email,
+                'username' :data.data.detail.username,
+                'password' : data.data.detail.password,
+                'foto_profil' : data.data.detail.foto_profil
+                }
+                $cookies.putObject('data',input);
+
             } else {
 				ErrMsg.alert();    
 				  
